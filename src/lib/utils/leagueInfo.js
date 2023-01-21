bfd30244a9b4e1c7f8da846a9802cc369c3f9fd5
Maxman3454 <123217575+Maxman3454@users.noreply.github.com>
2023-01-20 21:15:30 -0500

    Update leagueInfo.js

--- src/lib/utils/leagueInfo.js
+++ src/lib/utils/leagueInfo.js
@@ -23,27 +23,14 @@ https://github.com/nmelhado/league-page/blob/master/TRAINING_WHEELS.md#ii-adding
 
 export const managers = [
     {
-      "roster": 339998596667551744,  // ID of the roster that the manager manages (look at the order of the power rankings graph)
+      "roster": 339998596667551744, 
        "managerID": "339998596667551744",
       "Dexter McCann": "Your Name",
-      "tookOver": Null, // (optional) used if a manager took over a team, delete this line or change to null otherwise
-      "West Hartford/CHI Town": "Brooklyn", // (optional)
-      "bio": "Null.",
-      "photo": "/managers/Dex.jpg", // square ratio recommended (no larger than 500x500)
-      "fantasyStart": 2017, // (optional) when did the manager start playing fantasy football
-      "favoriteTeam": "nyg", // (optional) favorite NFL team, (follows convention: nyj, sea, mia, etc.) MUST BE LOWERCASE
-      "mode": "Win Now", // (optional) 'Win Now', 'Dynasty', or 'Rebuild' (anything else and you will need to add a new png to /static/ similar to the 'Rebuild.png' and 'Win Now.png' currently in there)
-      "rival": {
-        name: "Justin", // Can be anything (usually your rival's name)
-        link: Null, // manager array number within this array, or null to link back to all managers page
-        image: "/managers/Justin.jpg", // either a specific manager photo or '/managers/everyone.png' or '/managers/question.png'
-      },
-      "favoritePlayer": 4866, // (optional) this corresponds to the Sleeper player ID (https://api.sleeper.app/v1/players/nfl)
-      "valuePosition": "null", // (optional) Favorite position (QB, WR, RB, TE, etc.)
-      "rookieOrVets": "null", // (optional) 'Rookies' or 'Vets' (anything else and you will need to add a new png to /static/ similar to the 'Rookies.png' and 'Vets.png' currently in there)
-      "philosophy": "Your fantasy team's philosophy",
-      "tradingScale": 10, // 1 - 10
-      "preferredContact": "null", // 'Text', 'WhatsApp', 'Sleeper', 'Email', 'Phone', 'Discord', and 'Carrier Pigeon' are currently supplied in the template
+      "Location": "West Hartford/CHiTown", 
+      "photo": "/managers/Dex.jpg", 
+      "fantasyStart": 2017, 
+      "favoriteTeam": "nyg", 
+      "favoritePlayer": 4866, 
     },
     {
       "456876779588546560": 2,  // ID of the roster that the manager manages (look at the order of the power rankings graph)
@@ -89,6 +76,38 @@ export const managers = [
       "tradingScale": 2, // 1 - 10
       "preferredContact": "Null", // 'Text', 'WhatsApp', 'Sleeper', 'Email', 'Phone', 'Discord', and 'Carrier Pigeon' are currently supplied in the template
     },
+   {
+      "roster": 339998596667551744,  // ID of the roster that the manager manages (look at the order of the power rankings graph)
+       "managerID": "339998596667551744",
+      "Dexter McCann": "Your Name",
+      "tookOver": Null, // (optional) used if a manager took over a team, delete this line or change to null otherwise
+      "West Hartford/CHI Town": "Brooklyn", // (optional)
+      "bio": "Null.",
+      "photo": "/managers/Dex.jpg", // square ratio recommended (no larger than 500x500)
+      "fantasyStart": 2017, // (optional) when did the manager start playing fantasy football
+      "favoriteTeam": "nyg", // (optional) favorite NFL team, (follows convention: nyj, sea, mia, etc.) MUST BE LOWERCASE
+      "mode": "Win Now", // (optional) 'Win Now', 'Dynasty', or 'Rebuild' (anything else and you will need to add a new png to /static/ similar to the 'Rebuild.png' and 'Win Now.png' currently in there)
+      "rival": {
+        name: "Justin", // Can be anything (usually your rival's name)
+        link: Null, // manager array number within this array, or null to link back to all managers page
+        image: "/managers/Justin.jpg", // either a specific manager photo or '/managers/everyone.png' or '/managers/question.png'
+      },
+      {
+      "roster": 339998596667551744,  // ID of the roster that the manager manages (look at the order of the power rankings graph)
+       "managerID": "339998596667551744",
+      "Dexter McCann": "Your Name",
+      "tookOver": Null, // (optional) used if a manager took over a team, delete this line or change to null otherwise
+      "West Hartford/CHI Town": "Brooklyn", // (optional)
+      "bio": "Null.",
+      "photo": "/managers/Dex.jpg", // square ratio recommended (no larger than 500x500)
+      "fantasyStart": 2017, // (optional) when did the manager start playing fantasy football
+      "favoriteTeam": "nyg", // (optional) favorite NFL team, (follows convention: nyj, sea, mia, etc.) MUST BE LOWERCASE
+      "mode": "Win Now", // (optional) 'Win Now', 'Dynasty', or 'Rebuild' (anything else and you will need to add a new png to /static/ similar to the 'Rebuild.png' and 'Win Now.png' currently in there)
+      "rival": {
+        name: "Justin", // Can be anything (usually your rival's name)
+        link: Null, // manager array number within this array, or null to link back to all managers page
+        image: "/managers/Justin.jpg", // either a specific manager photo or '/managers/everyone.png' or '/managers/question.png'
+      },
   ]
   
   
