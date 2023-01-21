5e1b550b45635ae89dadebfbd5614331b046ff10
Maxman3454 <123217575+Maxman3454@users.noreply.github.com>
2023-01-20 21:54:42 -0500

    Update leagueInfo.js

--- src/lib/utils/leagueInfo.js
+++ src/lib/utils/leagueInfo.js
@@ -22,7 +22,15 @@ https://github.com/nmelhado/league-page/blob/master/TRAINING_WHEELS.md#ii-adding
 // To omit an optional field, set it's value to null
 
 export const managers = [
-    {
+   {
+       "managerID": "456659016873930752",  // the user's manager ID, go to https://api.sleeper.app/v1/league/<your_league_id>/users to find user IDs (you can use older leagueIDs to find user IDs for managers that are no longer in the league)
+       "name": "Max Berman",
+       "location": "Boston", // (optional)
+       "photo": "/managers/Max.jpg", // square ratio recommended (no larger than 500x500)
+      "favoriteTeam": "lvr", // (optional) favorite NFL team, (follows convention: nyj, sea, mia, etc.) MUST BE LOWERCASE
+       "favoritePlayer": 2028, // (optional) this corresponds to the Sleeper player ID (https://api.sleeper.app/v1/players/nfl)
+    },
+  {
        "managerID": "339998596667551744",  // the user's manager ID, go to https://api.sleeper.app/v1/league/<your_league_id>/users to find user IDs (you can use older leagueIDs to find user IDs for managers that are no longer in the league)
        "name": "Dexter McCann",
        "location": "ChiTown", // (optional)
