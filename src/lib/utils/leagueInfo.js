c568dff972b306a624e9c772e79ea9fd6319b78d
Maxman3454 <123217575+Maxman3454@users.noreply.github.com>
2025-08-12 12:05:45 -0400

    Update leagueInfo.js

--- src/lib/utils/leagueInfo.js
+++ src/lib/utils/leagueInfo.js
@@ -106,11 +106,11 @@ export const managers = [
        "location": "NYC", // (optional)
        "photo": "", // square ratio recommended (no larger than 500x500)
       "favoriteTeam": "buf", // (optional) favorite NFL team, (follows convention: nyj, sea, mia, etc.) MUST BE LOWERCASE
-      },
+   },
   {
     "managerID": "458148728809713664",  // the user's manager ID, go to https://api.sleeper.app/v1/league/<your_league_id>/users to find user IDs (you can use older leagueIDs to find user IDs for managers that are no longer in the league)
        "name": "Dylan",
        "location": "NYC", // (optional)
        "photo": "", // square ratio recommended (no larger than 500x500)
       "favoriteTeam": "nyg", // (optional) favorite NFL team, (follows convention: nyj, sea, mia, etc.) MUST BE LOWERCASE
-      },
+      }
